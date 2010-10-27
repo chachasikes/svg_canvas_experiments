8700c36e4a4abd3ed8b70ac4f74a3be2f7000033
chachasikes <chachasikes@gmail.com>
2010-10-27 01:19:37 -0500

    Got jquery svg to load an image and add stuff to it.

---
+++ js/jquery-svg-test.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var svg =  $('#svg-image').svg({
+    loadURL: 'images/diagram.svg',
+    onLoad: function(svg) { 
+      svg.rect(10, 20, 150, 100, {id: 'rect1', 
+          fill: 'red', stroke: 'black', strokeWidth: 3}); 
+      svg.circle(100, 150, 75, {id: 'circle1', 
+          fill: 'yellow', stroke: 'black', strokeWidth: 3}); 
+      var g = svg.group({transform: 'translate(200,20)'}); 
+      svg.circle(g, 100, 100, 75, {id: 'circle2', class_: 'grouped', 
+          fill: 'yellow', stroke: 'black', strokeWidth: 3}); 
+      svg.rect(g, 10, 150, 150, 100, {id: 'rect2', class_: 'grouped', 
+          fill: 'red', stroke: 'black', strokeWidth: 3});
+      svg.configure({offsetWidth: 300, offsetHeight: 300}, true);
+      }
+    });
+});
--- js/line.js
+++ js/line.js
@@ -1,39 +1,15 @@
-/*
-function draw_b() {
-  var b_canvas = document.getElementById("b");
-  var b_context = b_canvas.getContext("2d");
-  b_context.fillRect(50, 25, 150, 100);
+$(document).ready(function() {
+
+console.log(  $.data('svg') );
+
+});
+
+function drawLine() {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  context.fillRect(50, 25, 150, 100);
 }
-*/
 
- $(document).ready(function() {
-
-  // Draw a line when clicked.
-  $('a.drawline').click(drawLine);
-
-  function drawLine() {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    //context.fillRect(50, 25, 150, 100);
-
-    var x = 10;
-    var y = 10;
-
-  context.moveTo(0, y);
-  context.lineTo(500, y);
-  context.strokeStyle = "#aaa";
-  context.stroke();
-
-  /*
-  for (var y = 0.5; y < 375; y += 10) {
-    context.moveTo(0, y);
-    context.lineTo(500, y);
-    context.strokeStyle = "#eee";
-    context.stroke();
-  }
-  */
-
-
-  }
-
- });
+function clicked(){
+alert('hi');
+}
